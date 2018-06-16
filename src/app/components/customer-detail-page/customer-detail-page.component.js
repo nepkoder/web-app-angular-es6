4bf14dc621a253ef1a1ac1b3ce3e31f894c71cef
Prateek Jaiswal <prateek.jaiswal@shadowfax.in>
2018-06-16 21:38:03 +0530

    app structure

---
+++ src/app/components/customer-detail-page/customer-detail-page.component.js
@@ -0,0 +1,10 @@
+export class CustomerDetailPageCtrl {
+
+  constructor (toastr) {
+    'ngInject';
+
+    this.init_screen = "Hello SPA ES6";
+
+  }
+
+}
---
+++ src/app/components/overview-page/overview-page.component.js
@@ -0,0 +1,10 @@
+export class OverviewPageCtrl {
+
+  constructor (toastr) {
+    'ngInject';
+
+      this.init_screen = "Hello SPA ES6";
+
+  }
+
+}
