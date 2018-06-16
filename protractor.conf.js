6a11aa1da939fc8f8b18ded8e8a685c5a18d74ec
Prateek Jaiswal <prateek.jaiswal@shadowfax.in>
2018-06-17 02:37:12 +0530

    basic e2e jasmine tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: [paths.e2e + '/**/*.js'],
+  specs: [paths.src + '/app/components/**/*.spec.js'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
